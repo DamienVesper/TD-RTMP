f1481e8a449cf17655c57aac22d46977144074cf
Damien Vesper <ldamienvesper@gmail.com>
2021-06-03 18:54:34 -0500

    add more packages, start rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+extern crate bytes;
+extern crate slab;
+
+extern crate rml_rtmp;
+extern crate reqwest;
+
+mod log;
+
+mod connection;
+mod server;
+
+use std::collections::{ HashSet };
+use std::net::{ TcpListener, TcpStream };
+use std::sync::mpsc::{ channel, Receiver, TryRecvError };
+use std::thread;
+use slab::Slab;
+use ::connection::{ Connection, ConnectionError, ReadResult };
+use ::server::{ Server, ServerResult };
+
+fn main () {
+    let address = "0.0.0:1935";
+    let listener = TcpListener::bind(&address).unwrap();
+
+    let (stream_sender, stream_receiver) channel();
+
+    thread::spawn(|| { handle_connections(stream_receiver) });
+
+    log("Listening for connections on {}", address);
 }
