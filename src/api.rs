46db46805ce9b9f295a0ae59156ab8b03c9d5ae5
Rajeev Kapoor <44755991+MadhavWasTaken@users.noreply.github.com>
2021-06-08 00:10:13 +0530

    Ok, this works

--- src/api.rs
+++ src/api.rs
@@ -1,7 +1,8 @@
 use reqwest::Error;
 
 // GET REQUEST TO STREAM KEY API
-pub fn get_stream_key(streamkey: &str) -> Result<reqwest::blocking::Response, Error> {
-    let res = reqwest::blocking::get(format!("https://throwdown.tv/api/stream-key/{}", streamkey))?.json();
-    Ok(res.username)
+pub fn get_stream_key(streamkey: &str) -> Result<String, Error> {
+    let apikey = "uMGNMK323G1zOgyD2xBsuMGNMK323G13OgyD2xBsuMGNMK323G1zOgsd2xBs";
+    let res = reqwest::blocking::get(format!("https://throwdown.tv/api/stream-key/{}/{}",apikey, streamkey))?.text()?;
+    Ok(res)
 }
