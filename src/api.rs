8e13a4546078fc7c2732681bd5a68a23b8be1763
Rajeev Kapoor <44755991+MadhavWasTaken@users.noreply.github.com>
2021-06-11 23:40:41 +0530

    Yes, this works now

--- src/api.rs
+++ src/api.rs
@@ -1,17 +1,90 @@
-use std::collections::HashMap;
+use std::fs;
+use std::env;
+use std::process::Command;
+// GET REQUEST TO STREAM KEY API AND TRANSCODE IT WITH FFMPEG
 
-// GET REQUEST TO STREAM KEY API
+use serde_derive::{Deserialize, Serialize};
+#[derive(Serialize, Deserialize, Debug)]
+struct StreamInfo {
+    username: String,
+    verified: bool,
+    isSuspended: bool,
+    isVip: bool
+}
+
+// Start Stream
 #[tokio::main]
-pub async fn post(streamkey: &str) -> Result<(), Box<dyn std::error::Error>> { 
-    let client = reqwest::Client::new();
+pub async fn start(streamkey: &str) -> Result<(), Box<dyn std::error::Error>> { 
     let apikey = "uMGNMK323G1zOgyD2xBsuMGNMK323G13OgyD2xBsuMGNMK323G1zOgsd2xBs";
-    let mut map = HashMap::new();
-    map.insert("apiKey", apikey);
-    map.insert("streamKey", streamkey);
-    let res = client.post("http://localhost:8080/api/stream-key")
-        .json(&map)
-        .send()
+    let res_body = reqwest::get(format!("http://localhost:8080/api/stream-key/{}/{}", apikey, streamkey))
+        .await?
+        .text()
         .await?;
-    println!("BODY: {:?}", res);
+
+    let res: StreamInfo = serde_json::from_str(&res_body)?;
+    println!("CREATING HLS DIRECTORY FOR USERNAME: {}", res.username);
+
+    let mut directory: String = env::current_dir().unwrap().to_str().unwrap().to_string();
+    directory.push_str(&format!("/public/{}", res.username));
+
+    // Create Directory if not exists
+    fs::create_dir_all(&directory).ok();
+
+    Command::new("ffmpeg")
+        .arg("-v")
+        .arg("verbose")
+        .arg("-y")
+        .arg("-i")
+        .arg(format!("rtmp://127.0.0.1:1935/live/{}", streamkey))
+        .arg("-c:v")
+        .arg("libx264")
+        .arg("-c:a")
+        .arg("aac")
+        .arg("-ac")
+        .arg("1")
+        .arg("-strict")
+        .arg("-2")
+        .arg("-crf")
+        .arg("18")
+        .arg("-profile:v")
+        .arg("baseline")
+        .arg("-maxrate")
+        .arg("400k")
+        .arg("-bufsize")
+        .arg("1835k")
+        .arg("-pix_fmt")
+        .arg("yuv420p")
+        .arg("-flags")
+        .arg("-global_header")
+        .arg("-hls_time")
+        .arg("10")
+        .arg("-hls_list_size")
+        .arg("6")
+        .arg("-hls_wrap")
+        .arg("10")
+        .arg("-start_number")
+        .arg("1")
+        .arg(format!("{}/index.m3u8", directory))
+        .spawn()
+        .expect("ffmpeg does not exist");
+    Ok(())
+}
+
+#[tokio::main]
+pub async fn stop(streamkey: &str) -> Result<(), Box<dyn std::error::Error>> { 
+    let apikey = "uMGNMK323G1zOgyD2xBsuMGNMK323G13OgyD2xBsuMGNMK323G1zOgsd2xBs";
+    let res_body = reqwest::get(format!("http://localhost:8080/api/stream-key/{}/{}", apikey, streamkey))
+        .await?
+        .text()
+        .await?;
+
+    let res: StreamInfo = serde_json::from_str(&res_body)?;
+    println!("REMOVING HLS DIRECTORY FOR USERNAME: {}", res.username);
+
+    let mut directory: String = env::current_dir().unwrap().to_str().unwrap().to_string();
+    directory.push_str(&format!("/public/{}", res.username));
+
+    // Remove Directory if not exists
+    fs::remove_dir_all(&directory)?;
     Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -6,12 +6,10 @@ use rml_rtmp::sessions::{ServerSession, ServerSessionConfig, ServerSessionResult
 use rml_rtmp::sessions::StreamMetadata;
 use rml_rtmp::chunk_io::Packet;
 use rml_rtmp::time::RtmpTimestamp;
-use std::process::Command;
 use std::string::ToString;
 use std::string::String;
 use std::result::Result;
-use std::fs;
-use std::env;
+
 
 enum ClientAction {
     Waiting,
@@ -226,55 +224,8 @@ impl Server {
             }
         }
 
-        // FFMPEG TRANSCODE
-
-        crate::api::post(&stream_key);
-        let mut directory: String = env::current_dir().unwrap().to_str().unwrap().to_string();
-        let username: &str = "lightwarp";
-    
-        directory.push_str(&format!("/public/{}", username));
-        println!("{}", directory);
-
-        // Create Directory if not exists
-        fs::create_dir_all(&directory).ok();
-
-        Command::new("ffmpeg")
-            .arg("-v")
-            .arg("verbose")
-            .arg("-y")
-            .arg("-i")
-            .arg(format!("rtmp://127.0.0.1:1935/live/{}", stream_key))
-            .arg("-c:v")
-            .arg("libx264")
-            .arg("-c:a")
-            .arg("aac")
-            .arg("-ac")
-            .arg("1")
-            .arg("-strict")
-            .arg("-2")
-            .arg("-crf")
-            .arg("18")
-            .arg("-profile:v")
-            .arg("baseline")
-            .arg("-maxrate")
-            .arg("400k")
-            .arg("-bufsize")
-            .arg("1835k")
-            .arg("-pix_fmt")
-            .arg("yuv420p")
-            .arg("-flags")
-            .arg("-global_header")
-            .arg("-hls_time")
-            .arg("10")
-            .arg("-hls_list_size")
-            .arg("6")
-            .arg("-hls_wrap")
-            .arg("10")
-            .arg("-start_number")
-            .arg("1")
-            .arg(format!("{}/index.m3u8", directory))
-            .spawn()
-            .expect("ffmpeg does not exist");
+        // START FFMPEG TRANSCODE
+        crate::api::start(&stream_key);
 
         let accept_result;
         {
@@ -557,9 +508,10 @@ impl Server {
             Some(channel) => channel,
             None => return,
         };
-
         channel.publishing_client_id = None;
         channel.metadata = None;
+        // STOP FFMPEG TRANSCODE
+        crate::api::stop(&stream_key);
     }
 
     fn play_ended(&mut self, client_id: usize, stream_key: String) {
@@ -567,7 +519,6 @@ impl Server {
             Some(channel) => channel,
             None => return,
         };
-
         channel.watching_client_ids.remove(&client_id);
     }
 }
